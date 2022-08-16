b3da670f52b89ffcbeedd09b709504460fc39b1f
Evgenia Korystova <korystova@gmail.com>
2022-08-16 22:45:54 +0300

    Task13 completed

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int value = new Random().Next(0,10000);
+Console.WriteLine(value);
+int indexValue = 2;
+
+int digit = value.ToString()[indexValue] - '0';
+if(digit <= 9)
+{
+    Console.WriteLine(digit);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}

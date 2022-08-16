46ddd4982b83a98fc9e125a0c34fdb671f875544
Evgenia Korystova <korystova@gmail.com>
2022-08-16 21:36:21 +0300

    Task10 done

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int value = new Random().Next(100,1000);
+Console.WriteLine(value);
+int result1 = value / 10;
+int result2 = result1 % 10;
+Console.WriteLine(result2);

186985a589d23a7e7e0446084e442df40efe556c
Evgenia Korystova <korystova@gmail.com>
2022-09-20 07:32:11 +0300

    Completed task 15

---
+++ Task15/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write ("Введите число, соответсвующее номеру дня недели. Понедельник - 1, вторник - 2, среда - 3, четверг - 4, пятница - 5, суббота - 6, воскресенье - 7. Мы покажем Вам, выходной это день или нет:)");
+int value = Convert.ToInt32 (Console.ReadLine ());
+if (value == 1)
+{
+    Console.WriteLine ("НЕ ВЫХОДНОЙ");
+}
+if (value == 2)
+{
+    Console.WriteLine ("НЕ ВЫХОДНОЙ");
+}
+if (value == 3)
+{
+    Console.WriteLine ("НЕ ВЫХОДНОЙ");
+}
+if (value == 4)
+{
+    Console.WriteLine ("НЕ ВЫХОДНОЙ");
+}
+if (value == 5)
+{
+    Console.WriteLine ("НЕ ВЫХОДНОЙ");
+}
+if (value == 6)
+{
+    Console.WriteLine ("ВЫХОДНОЙ");
+}
+if (value == 7)
+{
+    Console.WriteLine ("ВЫХОДНОЙ");
+}
